78862a8355ed00c5481d67f25c1232951b74a324
Aguinaldo Muca Figueira <aguinaldomuca@gmail.com>
2025-08-24 15:58:34 +0100

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const moreProjects = () => {
+  const previewProject = document.querySelector(".moreProject");
+  previewProject.classList.toggle("hidden");
+};
+
+const morePlan = () => {
+  let openCloseMorePlan = document.querySelector(".mt-morePlan");
+  openCloseMorePlan.classList.toggle("hiddenTwo");
+};
